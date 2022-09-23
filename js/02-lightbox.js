58262a90961657be7e4a44f2269ac0d859d2a339
Serhei Honcharov <mrsedmor@gmail.com>
2022-09-23 17:28:20 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryNode = document.querySelector('.gallery');
+
+galleryNode.innerHTML = galleryItems.map(createGalleryImageMarkup).join('');
+
+function createGalleryImageMarkup({ preview, original, description }) {
+  const markup = `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+  return markup;
+}
+
+var lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
