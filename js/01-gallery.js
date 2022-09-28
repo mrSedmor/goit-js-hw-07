d452be08c85767b6972bc545fa02a6bb6b2015f2
Serhei Honcharov <mrsedmor@gmail.com>
2022-09-28 22:58:53 +0200

    Task 1 / new solution added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,26 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-// console.log(galleryItems);
+const galleryElem = document.querySelector('.gallery');
 
-const galleryNode = document.querySelector('.gallery');
+// Варіант №1
+galleryElem.innerHTML = createGalleryMarkup(galleryItems);
 
-galleryNode.innerHTML = galleryItems.map(createGalleryImageMarkup).join('');
-
-function createGalleryImageMarkup({ preview, original, description }) {
-  const markup = `<div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
-      </a>
-    </div>`;
-  return markup;
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+          </a>
+        </div>`;
+    })
+    .join('');
 }
 
-galleryNode.addEventListener('click', onGalleryClick);
+// Варіант №2
+// galleryElem.append(...createGalleryElements(galleryItems));
 
-function createLightboxMarkup({ source, description }) {
-  return `<img src="${source}" alt="${description}" />`;
-}
+// function createGalleryElements(gallaryItems) {
+//   return gallaryItems.map(({ preview, original, description }) => {
+//     const itemElem = document.createElement('div');
+//     itemElem.classList.add('gallery__item');
+
+//     const linkElem = document.createElement('a');
+//     linkElem.href = original;
+
+//     const imageElem = document.createElement('img');
+//     imageElem.classList.add('gallery__image');
+//     imageElem.src = preview;
+//     imageElem.dataset.source = original;
+//     imageElem.alt = description;
+
+//     linkElem.appendChild(imageElem);
+//     itemElem.appendChild(linkElem);
+
+//     return itemElem;
+//   });
+// }
+
+galleryElem.addEventListener('click', onGalleryClick);
 
 function onGalleryClick(event) {
   const image = event.target;
@@ -32,6 +54,8 @@ function onGalleryClick(event) {
 
   const source = image.dataset.source;
   const description = image.alt;
+
+  // Варіант №1
   const lightboxMarkup = createLightboxMarkup({ source, description });
 
   const lightbox = basicLightbox.create(lightboxMarkup, {
@@ -39,13 +63,38 @@ function onGalleryClick(event) {
     onClose: () => document.removeEventListener('keydown', onEscapePressed),
   });
 
+  // Варіант №2.
+  // const lightboxElement = createLightboxElement({ source, description });
+
+  // const lightbox = basicLightbox.create(lightboxElement, {
+  //   onShow: () => document.addEventListener('keydown', onEscapePressed),
+  //   onClose: () => document.removeEventListener('keydown', onEscapePressed),
+  // });
+
+  lightbox.show();
+
   function onEscapePressed(event) {
-    if (event.key !== 'Escape') {
+    if (event.code !== 'Escape') {
       return;
     }
 
     lightbox.close();
   }
-
-  lightbox.show();
 }
+
+// Варіант №1
+function createLightboxMarkup({ source, description }) {
+  return `<img src="${source}" alt="${description}" />`;
+}
+
+// Варіант №2
+// function createLightboxElement({ source, description }) {
+//   const imageElem = document.createElement('img');
+//   imageElem.src = source;
+//   imageElem.alt = description;
+
+//   const content = document.createElement('div');
+//   content.appendChild(imageElem);
+
+//   return content;
+// }
